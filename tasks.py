d8fa8c34925efebfabf22806940a1cffa51cfd0d
Tikka Lauri A <lauritik@cubbli24-gold.cs.helsinki.fi>
2026-03-31 14:21:11 +0300

    Modify tasks.py

--- tasks.py
+++ tasks.py
@@ -14,12 +14,12 @@ def coverage(ctx):
 
 @task(coverage)
 def coverage_report(ctx):
-    ctx.run("coverage html")
+    ctx.run("coverage html", pty=True)
 
 @task
 def lint(ctx):
-    ctx.run("pylint src")
+    ctx.run("pylint src", pty=True)
 
 @task
 def format(ctx):
-    ctx.run("autopep8 --in-place --recursive src")
+    ctx.run("autopep8 --in-place --recursive src", pty=True)
