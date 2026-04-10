f0acb8c08c667eb55a8e78380a7ed6594afef624
Lauri <tikkalauri1@gmail.com>
2026-04-10 17:06:46 +0300

    Add portal collision check method. Clears sprite groups, loads new sprites and updates level_id

--- src/level.py
+++ src/level.py
@@ -2,6 +2,7 @@ import pygame
 import json
 from sprites.player import Player
 from sprites.platforms import Platform, Floor
+from sprites.portal import Portal
 
 
 class Level:
@@ -11,6 +12,7 @@ class Level:
         self.floor = Floor()
         self.platforms = pygame.sprite.Group()
         self.all_sprites = pygame.sprite.Group()
+        self.portal = pygame.sprite.Group()
 
         self.all_sprites.add(
             self.player,
@@ -23,6 +25,7 @@ class Level:
         level_data = self.get_level_data()
 
         for dict in level_data["platforms"]:
+
             x = dict["plat_x"]
             y = dict["plat_y"]
 
@@ -30,11 +33,48 @@ class Level:
             self.platforms.add(platform)
             self.all_sprites.add(platform)
 
+        portal = Portal(level_data["portal"]["portal_x"],
+                        level_data["portal"]["portal_y"])
+        self.all_sprites.add(portal)
+        self.portal.add(portal)
+
     def get_groups(self):
         return self.all_sprites, self.platforms
-    
+
     def get_level_data(self):
         f = open(f"src/levels/level_{str(self.level_id)}.json")
         level_data = json.load(f)
 
         return level_data
+
+    def check_portal(self):
+        collisions = pygame.sprite.spritecollide(
+            self.player, self.portal, True)
+
+        if self.level_id == 1:
+            if collisions:
+                self.level_id = 2
+                self.clear_groups()
+                self.generate()
+                self.player.reset_pos()
+                return True
+
+        elif self.level_id == 2:
+            if collisions:
+                self.level_id = 1
+                self.clear_groups()
+                self.generate()
+                self.player.reset_pos()
+                return True
+
+    def clear_groups(self):
+        self.all_sprites.empty()
+        self.all_sprites.add(
+            self.player,
+            self.floor
+        )
+
+        self.platforms.empty()
+        self.platforms.add(self.floor)
+
+        self.portal.empty()
