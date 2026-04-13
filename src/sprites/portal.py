909ea6275b386a4feb2e7a55e79e97e5a2e459ce
Lauri <tikkalauri1@gmail.com>
2026-04-13 17:15:18 +0300

    remove comments

--- src/sprites/portal.py
+++ src/sprites/portal.py
@@ -4,7 +4,5 @@ import pygame
 class Portal(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
-        # self.surface = pygame.Surface((50, 50))
         self.surface = pygame.image.load("src/assets/portal.bmp")
-        # self.surface.fill((0, 255, 0))
         self.rect = self.surface.get_rect(midbottom=(x, y))
