88b56186b1190b8fbced0ace02ec06cdf9f1bc02
Tikka Lauri A <lauritik@cubbli24-gold.cs.helsinki.fi>
2026-04-16 15:43:20 +0300

    Format

--- src/sprites/rocket.py
+++ src/sprites/rocket.py
@@ -4,6 +4,6 @@ import pygame
 class Rocket(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
-        self.surface = pygame.surface.Surface((50,50))
-        self.surface.fill((255,0,0))
+        self.surface = pygame.surface.Surface((50, 50))
+        self.surface.fill((255, 0, 0))
         self.rect = self.surface.get_rect(midbottom=(x, y))
