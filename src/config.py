c77f8ba0f998af46f7d9bbd8175134e9104c81c4
Tikka Lauri A <lauritik@cubbli24-gold.cs.helsinki.fi>
2026-04-16 14:34:08 +0300

    Add variable for level count

--- src/config.py
+++ src/config.py
@@ -13,3 +13,5 @@ WELCOME_FONT = pygame.font.SysFont("arialblack", 50)
 PLAY_FONT = pygame.font.SysFont("arialblack", 60)
 WELCOME_COLOR = (255, 255, 255)
 PLAY_COLOR = (213, 216, 53)
+
+LEVEL_COUNT = 2
