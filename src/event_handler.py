e4bedf1c742428dc7177d82ced8139adaf4262db
Lauri <tikkalauri1@gmail.com>
2026-04-19 16:04:09 +0300

    Add MenuEventHandler-class for user input in menu

--- src/event_handler.py
+++ src/event_handler.py
@@ -29,3 +29,22 @@ class GameEventHandler:
 
     def update_platforms(self, platforms=pygame.sprite.Group):
         self.platforms = platforms
+
+
+class MenuEventHandler:
+    def handle_events(self, bottom_text, bottom_text_rect):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    if bottom_text_rect.collidepoint(event.pos):
+                        if bottom_text == "Main menu":
+                            return "menu"
+                        if bottom_text == "Play":
+                            return "game"
+
+    def is_hovering(self, bt_x, bt_y, bt_w, bt_h):
+        m_x, m_y = pygame.mouse.get_pos()
+        return bool(bt_x <= m_x <= bt_x + bt_w and bt_y <= m_y <= bt_y + bt_h)
