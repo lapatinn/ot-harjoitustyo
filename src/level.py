f26b56a1dc895e79febe5ac665b386e3752bcb4b
Lauri <tikkalauri1@gmail.com>
2026-04-23 14:46:36 +0300

    Add spike loading to levels and damage logic

--- src/level.py
+++ src/level.py
@@ -4,6 +4,7 @@ from sprites.player import Player
 from sprites.platforms import Platform, Floor
 from sprites.portal import Portal
 from sprites.rocket import Rocket
+from sprites.spike import Spike
 from config import LEVEL_COUNT
 
 
@@ -16,6 +17,7 @@ class Level:
         self.all_sprites = pygame.sprite.Group()
         self.portal = pygame.sprite.Group()
         self.rocket = pygame.sprite.Group()
+        self.spikes = pygame.sprite.Group()
 
         self.all_sprites.add(
             self.player,
@@ -50,6 +52,13 @@ class Level:
             self.all_sprites.add(rocket)
             self.rocket.add(rocket)
 
+        if "spikes" in level_data:
+            for dict in level_data["spikes"]:
+                spike = Spike(dict["spike_x"], dict["spike_y"])
+
+                self.spikes.add(spike)
+                self.all_sprites.add(spike)
+
     def get_groups(self):
         return self.all_sprites, self.platforms
 
@@ -77,6 +86,15 @@ class Level:
 
         if collisions:
             return True
+
+    def check_damage(self):
+        collisions = pygame.sprite.spritecollide(
+            self.player, self.spikes, True)
+
+        if collisions:
+            self.player.hit()
+            if self.player.health < 1:
+                return True
 
     def clear_groups(self):
         self.all_sprites.empty()
